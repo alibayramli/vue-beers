f5b69cc027e5b78f53c46453a53eb7345d806518
Ali Bayramli <alibayramli97@gmail.com>
2020-07-05 14:14:13 +0200

    Add hero component

--- index.js
+++ index.js
@@ -56,6 +56,37 @@ const navigationComponent = {
     </v-app-bar>`
 }
 
+const heroComponent = {
+    template: `
+    <section id="hero">
+        <v-row no-gutters>
+            <v-img :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+                src="https://images.unsplash.com/photo-1504502350688-00f5d59bbdeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80%20750w">
+                <v-theme-provider dark>
+                    <v-container fill-height>
+                        <v-row align="center" class="white--text mx-auto" justify="center">
+                            <v-col class="white--text text-center" cols="12" tag="h1">
+                                <span :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+                                    class="font-weight-light">
+                                    WELCOME TO
+                                </span>
+                                <br>
+                                <span :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
+                                    class="font-weight-black">
+                                    Beer Factory
+                                </span>
+                            </v-col>
+                            <v-btn class="align-self-end" fab outlined @click="$vuetify.goTo('#about')">
+                                <v-icon>mdi-chevron-double-down</v-icon>
+                            </v-btn>
+                        </v-row>
+                    </v-container>
+                </v-theme-provider>
+            </v-img>
+        </v-row>
+    </section>`
+}
+
 const randomBeersComponent = {
     beforeMount() {
         this.$store.dispatch("getAllBeers");
@@ -287,5 +318,5 @@ new Vue({
     el: '#app',
     store: store,
     vuetify: new Vuetify(),
-    components: { navigationComponent, randomBeersComponent, statsComponent, queryFormComponent, queryCardComponent },
+    components: { navigationComponent, heroComponent, randomBeersComponent, statsComponent, queryFormComponent, queryCardComponent },
 })
