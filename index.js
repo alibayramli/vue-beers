22161eb558a910ac201097271ef245109b996301
Ali Bayramli <alibayramli97@gmail.com>
2020-07-05 18:40:00 +0200

    Move info section to randombeer component

--- index.js
+++ index.js
@@ -130,51 +130,57 @@ const randomBeersComponent = {
         },
     },
     template: ` 
-    <div>
-        <h2 class="display-2 font-weight-bold mb-3">LETS GIVE YOU FACTS</h2>
-        <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720" v-if="randomBeer.length < 3">
-            Click on the button to get random beers from the database, learn a few useful things about
-            them,
-            you can thank me later 😉
-        </v-responsive>
-        <v-btn color="blue" href="" large @click="getRandomBeer" v-if = "randomBeer.length < 3">
-            <span class="white--text text--darken-1 font-weight-bold ">
-                Get beers
-            </span>
-        </v-btn>
-        <div class="py-2"></div>
-        <div v-if="randomBeer.length === 3">
-            <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720">
-                Great! Seems you enjoy viewing, click on advanced query button to start filtering values or go to stats to see what you have logged so far
-            </v-responsive>
-            <v-btn color="blue" class='my-2' href="" large @click="$vuetify.goTo('#advanced-query')" >
-                <span class="white--text text--darken-1 font-weight-bold ">
-                    Advanced query
-                </span>
-            </v-btn>
-            <v-btn color="green" href="" large @click="$vuetify.goTo('#stats')" >
-                <span class="white--text text--darken-1 font-weight-bold ">
-                    Check your stats
-                </span>
-            </v-btn>
-        </div>
-        <v-responsive class="mx-auto mb-12" width="56"></v-responsive>
-        <v-row>
-            <v-col v-for="({ icon, name, description, tips }, i) in randomBeer" :key="i" cols="12" md="4">
-                <v-card class="py-12 px-4" color="grey lighten-5" flat  height="100%">
-                    <v-theme-provider dark>
-                        <v-avatar color="primary" size="88">
-                            <v-icon large v-text="icon"></v-icon>
-                        </v-avatar>
-                    </v-theme-provider>
-                    <v-card-title class="justify-center text-uppercase" v-text="name"></v-card-title>   
-                    <hr>
-                    <v-card-text class="subtitle-1" v-text="description"></v-card-text> <hr>
-                    <h4 class="title" style="color:grey"><b>tips:</b> <span class="subtitle-1" v-text="tips"></span></h4>
-                </v-card>
-            </v-col>
-        </v-row>
-    </div>`
+    <section id="info" class="grey lighten-3">
+        <div class="py-12"></div>
+        <v-container class="text-center">
+            <div>
+                <h2 class="display-2 font-weight-bold mb-3">LETS GIVE YOU FACTS</h2>
+                <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720" v-if="randomBeer.length < 3">
+                    Click on the button to get random beers from the database, learn a few useful things about
+                    them,
+                    you can thank me later 😉
+                </v-responsive>
+                <v-btn color="blue" href="" large @click="getRandomBeer" v-if = "randomBeer.length < 3">
+                    <span class="white--text text--darken-1 font-weight-bold ">
+                        Get beers
+                    </span>
+                </v-btn>
+                <div class="py-2"></div>
+                <div v-if="randomBeer.length === 3">
+                    <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720">
+                        Great! Seems you enjoy viewing, click on advanced query button to start filtering values or go to stats to see what you have logged so far
+                    </v-responsive>
+                    <v-btn color="blue" class='my-2' href="" large @click="$vuetify.goTo('#advanced-query')" >
+                        <span class="white--text text--darken-1 font-weight-bold ">
+                            Advanced query
+                        </span>
+                    </v-btn>
+                    <v-btn color="green" href="" large @click="$vuetify.goTo('#stats')" >
+                        <span class="white--text text--darken-1 font-weight-bold ">
+                            Check your stats
+                        </span>
+                    </v-btn>
+                </div>
+                <v-row>
+                    <v-col v-for="({ icon, name, description, tips }, i) in randomBeer" :key="i" cols="12" md="4">
+                        <v-card class="py-12 px-4" color="grey lighten-5" flat  height="100%">
+                            <v-theme-provider dark>
+                                <v-avatar color="primary" size="88">
+                                    <v-icon large v-text="icon"></v-icon>
+                                </v-avatar>
+                            </v-theme-provider>
+                            <v-card-title class="justify-center text-uppercase" v-text="name"></v-card-title>   
+                            <hr>
+                            <v-card-text class="subtitle-1" v-text="description"></v-card-text> <hr>
+                            <h4 class="title" style="color:grey"><b>tips:</b> <span class="subtitle-1" v-text="tips"></span></h4>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-container>
+        <div class="py-12"></div>
+    </section>
+   `
 }
 
 const queryFormComponent = {
@@ -234,7 +240,50 @@ const queryFormComponent = {
             </v-layout>
             <div class="py-12"></div>
         </v-container>
-</v-form>`
+    </v-form>`
+}
+
+const queryCardComponent = {
+    computed: {
+        queryResults: function () {
+            return this.$store.state.queryResults;
+        },
+    },
+    template: `
+        <v-simple-table height="300px" >
+            <template v-slot:default>
+                <thead v-if="queryResults.length !== 0">
+                    <tr>
+                    <th class="text-left">Row</th>
+                    <th class="text-left">Id</th>
+                    <th class="text-left">Name</th>
+                    <th class="text-left">Abv</th>
+                    <th class="text-left">Ebc</th>
+                    <th class="text-left">Ibu</th>
+                    <th class="text-left">Year brewed</th>
+                    <th class="text-left">Food Match</th>
+                    <th class="text-left">Image</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item,index) in queryResults" :key="item.name">
+                    <td>{{ index + 1 }}</td>
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.abv }}</td>
+                    <td>{{ item.ebc || 0 }}</td>
+                    <td>{{ item.ibu || 0 }}</td>
+                    <td>{{ item.first_brewed }}</td>
+                    <td>{{ item.food_pairing[0] }}</td>
+                    <td v-if="item.image_url"> <a v-bind:href="item.image_url" style='text-decoration:none' target='_blank'>url</a></td>
+                    </tr>
+                </tbody>
+                <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720" v-if="queryResults.length === 0">
+                    Fetching no data :((
+                </v-responsive>
+            </template>
+        </v-simple-table>
+   `
 }
 
 const statsComponent = {
@@ -299,48 +348,7 @@ const statsComponent = {
     </v-parallax>`
 }
 
-const queryCardComponent = {
-    computed: {
-        queryResults: function () {
-            return this.$store.state.queryResults;
-        },
-    },
-    template: `
-        <v-simple-table height="300px" >
-            <template v-slot:default>
-                <thead v-if="queryResults.length !== 0">
-                    <tr>
-                    <th class="text-left">Row</th>
-                    <th class="text-left">Id</th>
-                    <th class="text-left">Name</th>
-                    <th class="text-left">Abv</th>
-                    <th class="text-left">Ebc</th>
-                    <th class="text-left">Ibu</th>
-                    <th class="text-left">Year brewed</th>
-                    <th class="text-left">Food Match</th>
-                    <th class="text-left">Image</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(item,index) in queryResults" :key="item.name">
-                    <td>{{ index + 1 }}</td>
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.abv }}</td>
-                    <td>{{ item.ebc || 0 }}</td>
-                    <td>{{ item.ibu || 0 }}</td>
-                    <td>{{ item.first_brewed }}</td>
-                    <td>{{ item.food_pairing[0] }}</td>
-                    <td v-if="item.image_url"> <a v-bind:href="item.image_url" style='text-decoration:none' target='_blank'>url</a></td>
-                    </tr>
-                </tbody>
-                <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720" v-if="queryResults.length === 0">
-                    Fetching no data :((
-                </v-responsive>
-            </template>
-        </v-simple-table>
-   `
-}
+
 
 new Vue({
     el: '#app',
